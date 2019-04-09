3599c29e1732126a02dbd6a7b52823a746946304
Anton Dombrovksyi <antonio107207@gmail.com>
2019-04-09 19:30:35 +0300

    commit

---
+++ routs/apiRouts.js
@@ -0,0 +1,10 @@
+const {
+    getOffers, getOffersPerDay, getOffer
+} = require('../controller/apiController.js');
+
+module.exports = function (router) {
+    router
+        .get('/api/get-offers', getOffers)
+        .get('/api/get-offers-per-day', getOffersPerDay)
+        .get('/api/get-offer/:id', getOffer);
+};
